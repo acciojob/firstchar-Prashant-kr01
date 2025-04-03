4d88b544e6ad59375c84232bf238248ae50ed345
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-04-03 19:59:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstChar(text) {
   // your code here
-	  let char  text.trimStart();
-       return  char[0];
+	  let char = text.trimStart();
+	
+	 if (char.length === 0) {
+        return ''; 
 }
+       return  char[0];
+
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
